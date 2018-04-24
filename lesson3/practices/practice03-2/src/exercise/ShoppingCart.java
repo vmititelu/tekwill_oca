c1ec5b0ea97a5c2ba1bf57e99526e86b720db988
vmititelu <vitalie.mititelu@gmail.com>
2018-04-24 20:23:39 +0300

    my first project

---
+++ lesson3/practices/practice03-2/src/exercise/ShoppingCart.java
@@ -0,0 +1,7 @@
+/* Copyright © 2016 Oracle and/or its affiliates. All rights reserved. */
+
+package exercise;
+
+public class ShoppingCart {
+    
+}
---
+++ lesson3/src/myfirstpackage/MyFirstClass.java
@@ -0,0 +1,14 @@
+package myfirstpackage;
+
+public class MyFirstClass {
+
+    public static void main(String[] args) {
+        int total = sum(1, 2);
+        System.out.println("My First java application " + total);
+    }
+
+    public static int sum (int x, int y){
+        return x + y;
+    }
+
+}
